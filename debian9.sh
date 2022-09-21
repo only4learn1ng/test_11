30d81d54a5eb08541bae62d85748418cc35f09ec
only4learn1ng <77775090+only4learn1ng@users.noreply.github.com>
2022-09-22 01:34:38 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+rm /etc/apt/sources.list
+wget https://raw.githubusercontent.com/only4learn1ng/test_11/main/sources.list
+mv sources.list /etc/apt/sources.list
+apt update
+apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
