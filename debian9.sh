f6a836d1a3b928ab4b834aebfe9c051cdae169e0
only4learn1ng <77775090+only4learn1ng@users.noreply.github.com>
2022-09-22 01:22:21 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i 's/stretch/buster/g' /etc/apt/sources.list
-apt update
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
