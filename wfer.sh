4d0c0823c396f376a950da503828d65c2f466ea6
only4learn1ng <77775090+only4learn1ng@users.noreply.github.com>
2022-09-22 01:08:04 +0800

    Update wfer.sh

--- wfer.sh
+++ wfer.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+sed -i 's/stretch/buster/g' /etc/apt/sources.list
 apt update
-curl -fsSL https://get.docker.com -o get-docker.sh
-bash get-docker.sh
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt install -y docker-ce
+
 curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
