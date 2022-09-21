06a8a2357cd51d8104c0956d0f41101097527c59
only4learn1ng <77775090+only4learn1ng@users.noreply.github.com>
2022-09-22 00:44:16 +0800

    Update wfer.sh

--- wfer.sh
+++ wfer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo bash get-docker.sh
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh
+bash get-docker.sh
+curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 docker run -d \
     --name $4 \
